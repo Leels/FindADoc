89de2f644da91f5734cc7cd252893cc7f4c94f05
Lela <smith.lelaa@gmail.com>
2019-12-09 11:41:05 -0800

    add !response.ok to api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,6 +2,9 @@ export class BetterDoctorApi {
   async getApiData(concern, name) {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${concern}&name=${name}&location=or-portland&user_location=45.505%2C-122.675&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      if (!response.ok) {
+        throw response
+      }
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
@@ -15,6 +18,9 @@ export class GiphyApi {
   async getGiphyApiData() {
     try {
       let response2 = await fetch(`https://api.giphy.com/v1/gifs/A9MftKr3J3lra?api_key=${process.env.OTHER_API_KEY}`);
+      if (!response.ok) {
+        throw response
+      }
       let jsonifiedResponse2 = await response2.json();
       return jsonifiedResponse2;
     } catch(error) {
